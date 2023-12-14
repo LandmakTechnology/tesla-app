23879e184dd1b7a47d31d4d47e9fdc2d34f5529a
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-12-14 12:58:40 -0800

    Update konoha_svc.tf

--- konoha_svc.tf
+++ konoha_svc.tf
@@ -14,27 +14,3 @@ resource "kubernetes_service_v1" "konoha-svc" {
     type = "NodePort"
   }
 }
-
-resource "kubernetes_service_v1" "arsenal-svc" {
-  metadata {
-    name = "arsenal-svc"
-    #annotations = [
-                    #"service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
-                    #"service.beta.kubernetes.io/aws-load-balancer-scheme" =  "internet-facing"
-                    #"service.beta.kubernetes.io/aws-load-balancer-name" = "arsenal-nlb"
-                  #]
-  }
-  spec {
-    selector = {
-      app = "arsenal"
-    }
-    port {
-      port        = 80
-      target_port = 80
-    }
-
-    type = "NodePort"
-  }
-}
-
-#Note Last attempt at practicing NLB service annotations and LoadBalancer did not work as of 12/5/23 but ALB and Nginx+NLB ingress works well.
