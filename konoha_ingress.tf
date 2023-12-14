fa5425050518e5647306bf49f15cc4d520c2359e
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-12-14 12:52:51 -0800

    Update konoha_ingress.tf

--- konoha_ingress.tf
+++ konoha_ingress.tf
@@ -45,7 +45,7 @@ resource "kubernetes_ingress_v1" "konoha" {
         "alb.ingress.kubernetes.io/load-balancer-name" = "konoha"
         "alb.ingress.kubernetes.io/scheme"             = "internet-facing"
         "alb.ingress.kubernetes.io/target-type"        = "ip"                                       
-        "alb.ingress.kubernetes.io/certificate-arn" = "arn:aws:acm:us-west-2:083772204804:certificate/58e604e3-40aa-4644-b0ad-8663f8e67c2a"
+        "alb.ingress.kubernetes.io/certificate-arn" = "arn:aws:acm:us-west-2:083772204804:certificate/069afb91-11fb-41e9-9044-7b0daf2cb6a1"
   }
   }
   spec {
