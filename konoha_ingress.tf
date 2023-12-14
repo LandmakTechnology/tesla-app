7b3fd8f6053243c59cb3749d0bf878843e9e7c38
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-12-14 13:15:39 -0800

    Update konoha_ingress.tf

--- konoha_ingress.tf
+++ konoha_ingress.tf
@@ -1,69 +1,25 @@
 ##NGINX
 
-#resource "kubernetes_ingress_v1" "arsenal-ingress" {
-#  wait_for_load_balancer = true
-#  metadata {
-#    name = "arsenal"
-#    namespace = "default"
-#    annotations = {
-#        "cert-manager.io/cluster-issuer" = "arsenal-issuer1"
-#  }
-#  }
-#  spec {
-#    ingress_class_name = "nginx"
-#    tls {
-#      secret_name = "arsenal-secret"
-#      hosts = ["arsenal.devopsnetwork.net"] 
-#    }
-#    rule {
-#      host = "arsenal.devopsnetwork.net"  
-#      http {
-#        path {
-#          path = "/"
-#          backend {
-#            service {
-#              name = "arsenal-svc"
-#              port {
-#                number = 80
-#              }
-#            }
-#          }
-#        }
-#      }
-#    }
-#  }
-#}
-
-##ALB
-
-resource "kubernetes_ingress_v1" "konoha" {
+resource "kubernetes_ingress_v1" "konoha-ingress" {
   wait_for_load_balancer = true
   metadata {
     name = "konoha"
     namespace = "default"
     annotations = {
-        "alb.ingress.kubernetes.io/load-balancer-name" = "konoha"
-        "alb.ingress.kubernetes.io/scheme"             = "internet-facing"
-        "alb.ingress.kubernetes.io/target-type"        = "ip"                                       
-        "alb.ingress.kubernetes.io/certificate-arn" = "arn:aws:acm:us-west-2:083772204804:certificate/069afb91-11fb-41e9-9044-7b0daf2cb6a1"
+        "cert-manager.io/cluster-issuer" = "konoha-issuer"
   }
   }
   spec {
-    ingress_class_name = "alb"
-    default_backend {
-      service {
-        name = "konoha-svc"
-        port {
-          number = 80
-        }
-      }
+    ingress_class_name = "nginx"
+    tls {
+      secret_name = "konoha-secret"
+      hosts = ["konoha.devopsnetwork.net"] 
     }
     rule {
       host = "konoha.devopsnetwork.net"  
       http {
         path {
           path = "/"
-          path_type = "Exact"
           backend {
             service {
               name = "konoha-svc"
@@ -77,3 +33,47 @@ resource "kubernetes_ingress_v1" "konoha" {
     }
   }
 }
+
+##ALB
+
+#resource "kubernetes_ingress_v1" "konoha" {
+#  wait_for_load_balancer = true
+#  metadata {
+#    name = "konoha"
+#    namespace = "default"
+#    annotations = {
+#        "alb.ingress.kubernetes.io/load-balancer-name" = "konoha"
+#        "alb.ingress.kubernetes.io/scheme"             = "internet-facing"
+#        "alb.ingress.kubernetes.io/target-type"        = "ip"                                       
+#        "alb.ingress.kubernetes.io/certificate-arn" = "arn:aws:acm:us-west-2:083772204804:certificate/069afb91-11fb-41e9-9044-7b0daf2cb6a1"
+#  }
+#  }
+#  spec {
+#    ingress_class_name = "alb"
+#    default_backend {
+#      service {
+#        name = "konoha-svc"
+#        port {
+#          number = 80
+#        }
+#      }
+#    }
+#    rule {
+#      host = "konoha.devopsnetwork.net"  
+#      http {
+#        path {
+#          path = "/"
+#          path_type = "Exact"
+#          backend {
+#            service {
+#              name = "konoha-svc"
+#              port {
+#                number = 80
+#              }
+#            }
+#          }
+#        }
+#      }
+#    }
+#  }
+#}
